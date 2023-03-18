1c939d70ddf36a83f24a5cb7b917aade9d8bb953
Your Name <you@example.com>
2023-03-18 23:29:56 +0530

    skill

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 document.addEventListener('scroll',()=>{
     const header = document.querySelector('nav')
     const nav=document.querySelector(".nav")
-    // const img=document.querySelector("#home")
+    const img=document.querySelector("#home")
+    const skill=document.querySelector("#skill")
     if (window.scrollY>0) {
         header.classList.add('scroll')
         nav.classList.add('scroll')
-        // img.style.marginTop="100px"
+        img.style.paddingTop="100px"
+        skill.style.paddingTop="100px"
+        img.style.webkitTransition="all 0.5s ease-in"
     }else{
         header.classList.remove('scroll')
         nav.classList.remove('scroll')
-        // img.style.marginTop="0px"
+        img.style.paddingTop="0px"
     }
 })
+
+
+
+
+
