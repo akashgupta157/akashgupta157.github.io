3b8d8fa51d3334a91b3efd64e8bdca45ac42b94d
Your Name <you@example.com>
2023-03-18 14:05:33 +0530

    profile

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.addEventListener('scroll',()=>{
     const header = document.querySelector('nav')
     const nav=document.querySelector(".nav")
+    // const img=document.querySelector("#home")
     if (window.scrollY>0) {
         header.classList.add('scroll')
         nav.classList.add('scroll')
+        // img.style.marginTop="100px"
     }else{
         header.classList.remove('scroll')
         nav.classList.remove('scroll')
+        // img.style.marginTop="0px"
     }
 })
