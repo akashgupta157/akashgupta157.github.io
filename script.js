9d1efb2e89253e786927fe818ae47ff43bd78f44
Your Name <you@example.com>
2023-04-27 22:20:27 +0530

    skill

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ click.onclick = () => {
     hbc.style.display = "none";
   }
 };
+var myNav = document.getElementById("nav-menu");
+var tablet = window.matchMedia("(max-width:768px)")
+var mobile = window.matchMedia("(max-width:480px)")
+window.onscroll = () => {
+  if (document.documentElement.scrollTop >= 10) {
+    myNav.style.backgroundColor = "#3a89d0";
+    if(tablet.matches || mobile.matches){
+      myNav.style.backgroundColor="transparent"
+    }
+  }else{
+    myNav.style.backgroundColor = "transparent";
+  }
+};
