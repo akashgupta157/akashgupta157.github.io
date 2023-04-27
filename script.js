4fb9a95221b3e145edd45f27e20a3513b12aad0c
Your Name <you@example.com>
2023-04-27 14:56:57 +0530

    nav

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function NewTab() {
   window.open("Akash-Gupta-Resume.pdf", "_blank");
 }
+let click = document.getElementById("click");
+let hbo = document.getElementById("hbo");
+let hbc = document.getElementById("hbc");
+click.onclick = () => {
+  if (click.checked) {
+    hbo.style.display = "none";
+    hbc.style.display = "flex";
+  } else {
+    hbo.style.display = "flex";
+    hbc.style.display = "none";
+  }
+};
