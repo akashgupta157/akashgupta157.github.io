4f5b64a56f3f95c579f7bfd28ae69a702a3b6dd3
Your Name <you@example.com>
2023-05-01 17:38:54 +0530

    a

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function NewTab() {
-  window.open("Akash-Gupta-Resume.pdf", "_blank");
-}
+let react = document.getElementById("react");
+let mui = document.getElementById("mui");
+let njs = document.getElementById("njs");
+let mdb = document.getElementById("mdb");
+let git = document.getElementById("git");
+let github = document.getElementById("github");
 let click = document.getElementById("click");
 let hbo = document.getElementById("hbo");
 let hbc = document.getElementById("hbc");
+let tablet = window.matchMedia("(max-width:768px)");
+let mobile = window.matchMedia("(max-width:480px)");
+let home = document.querySelector(".home");
+let about = document.querySelector(".about");
+let skills = document.querySelector(".skills");
+let projects = document.querySelector(".projects");
+let contact = document.querySelector(".contact");
 click.onclick = () => {
   if (click.checked) {
     hbo.style.display = "none";
@@ -13,16 +23,56 @@ click.onclick = () => {
     hbc.style.display = "none";
   }
 };
+if (mobile.matches || tablet.matches) {
+  home.onclick = () => {
+    click.checked = false;
+    hbo.style.display = "flex";
+    hbc.style.display = "none";
+  };
+  about.onclick = () => {
+    click.checked = false;
+    hbo.style.display = "flex";
+    hbc.style.display = "none";
+  };
+  skills.onclick = () => {
+    click.checked = false;
+    hbo.style.display = "flex";
+    hbc.style.display = "none";
+  };
+  projects.onclick = () => {
+    click.checked = false;
+    hbo.style.display = "flex";
+    hbc.style.display = "none";
+  };
+  contact.onclick = () => {
+    click.checked = false;
+    hbo.style.display = "flex";
+    hbc.style.display = "none";
+  };
+  react.setAttribute("data-aos", "fade-right");
+  mui.setAttribute("data-aos", "fade-left");
+  njs.setAttribute("data-aos", "fade-left");
+  mdb.setAttribute("data-aos", "fade-right");
+  git.setAttribute("data-aos", "fade-right");
+  github.setAttribute("data-aos", "fade-right");
+}
+function NewTab() {
+  window.open("Akash-Gupta-Resume.pdf", "_blank");
+  click.checked = false;
+  hbo.style.display = "flex";
+  hbc.style.display = "none";
+}
 var myNav = document.getElementById("nav-menu");
-var tablet = window.matchMedia("(max-width:768px)")
-var mobile = window.matchMedia("(max-width:480px)")
 window.onscroll = () => {
   if (document.documentElement.scrollTop >= 10) {
-    myNav.style.backgroundColor = "#3a89d0";
-    if(tablet.matches || mobile.matches){
-      myNav.style.backgroundColor="transparent"
+    myNav.style.backgroundColor = "white";
+    myNav.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px";
+    if (tablet.matches || mobile.matches) {
+      myNav.style.backgroundColor = "transparent";
+      myNav.style.boxShadow = "none";
     }
-  }else{
+  } else {
+    myNav.style.boxShadow = "none";
     myNav.style.backgroundColor = "transparent";
   }
 };
